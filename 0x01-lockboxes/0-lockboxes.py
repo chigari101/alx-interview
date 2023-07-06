33f9d07300e09a241113f34d0424a72ec2b189d3
chigari101 <chigari101@gmail.com>
2023-07-06 14:03:40 -0700

    added files on lockboxes project

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,55 @@
 #!/usr/bin/python3
+"""Solves the lock boxes puzzle """
+
+
+def look_next_opened_box(opened_boxes):
+
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
 
-    num_boxes = len(boxes)
-    visited = [False] * num_boxes
-    visited[0] = True
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
 
-    queue = [0]
-    while queue:
-        curr_box = queue.pop(0)
-        keys = boxes[curr_box]
-        for key in keys:
-            if 0 <= key < num_boxes and not visited[key]:
-                visited[key] = True
-                queue.append(key)
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
 
-    return all(visited)
+    return len(aux) == len(boxes)
 
+
+def main():
+    """Entry point"""
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
