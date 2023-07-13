11d82ebfa2795826cf7e63d05d0c81a0dba95065
chigari101 <chigari101@gmail.com>
2023-07-13 10:10:22 -0700

    added files on minimum operations project.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 
 """
     In a text file, there is a single character H.
-    The text editor can execute only two operations in this file: 
+    The text editor can execute only two operations in this file:
     Given a number n, this a script that write a method that calculates the fewest number
     of operations needed to result in exactly n H characters in the file.
 """
