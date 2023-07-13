67be1f4d7f7e8d26733bf32ca06878cdeb602bf1
chigari101 <chigari101@gmail.com>
2023-07-13 06:48:47 -0700

    added files on minimum operation project

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    if n == 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+
+# Test the function
+n = 9
+result = minOperations(n)
+print("Number of operations:", result)
