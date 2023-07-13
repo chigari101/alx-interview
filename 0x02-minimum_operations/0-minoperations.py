10b3526a9a4a395da35ff18ed5a21df9fdd48cdc
chigari101 <chigari101@gmail.com>
2023-07-13 10:01:27 -0700

    added files on minimum operations project.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 
+
+"""
+    In a text file, there is a single character H.
+    The text editor can execute only two operations in this file: 
+    Given a number n, this a script that write a method that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file.
+"""
+
+
 def minOperations(n):
-    if n == 1:
-        return 0
-
-    operations = 0
-    divisor = 2
-
+    nope = 0
+    minope = 2
     while n > 1:
-        if n % divisor == 0:
-            operations += divisor
-            n /= divisor
-        else:
-            divisor += 1
-
-    return operations
-
-
-if __name__ == "__main__":
-    n = 4
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+        while n % minope == 0:
+            nope += minope
+            n /= minope
+        minope += 1
+    return nope
