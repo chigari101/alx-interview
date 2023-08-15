5830d9d46c9841e53c967d06f900d79969315b9f
chigari101 <chigari101@gmail.com>
2023-08-15 06:23:01 -0700

    added files

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
+"""Rotates 2-D matrix"""
+
 
 def rotate_2d_matrix(matrix):
-    n = len(matrix)
+    """Rotates in-place"""
+    row = len(matrix)
+    column = row - 1
 
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-    # Reverse each row
-    for i in range(n):
-        matrix[i].reverse()
-
-
-# Example usage
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    for i in range(0, int(row / 2)):
+        for j in range(i, column - i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[column - j][i]
+            matrix[column - j][i] = matrix[column - i][column - j]
+            matrix[column - i][column - j] = matrix[j][column - i]
+            matrix[j][column - i] = temp
