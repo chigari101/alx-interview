e3788e276c1f0f3f1e4a2b8efa301a3e44bb5ab4
chigari101 <chigari101@gmail.com>
2023-09-01 06:15:01 -0700

    added file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-
-"""0x09. Island Perimeter """
+"""
+Island perimeter computing.
+"""
 
 
 def island_perimeter(grid):
-    if not grid:
+    """
+    Computes the perimeter of an island with no lakes.
+    """
+    perimeter = 0
+
+    if type(grid) != list:
         return 0
 
-    perimeter = 0
-    rows, cols = len(grid), len(grid[0])
+    n = len(grid)
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                # Start with the assumption that its a land cell
+    for i, row in enumerate(grid):
+        m = len(row)
 
-                # Check neighboring cells (up, down, left, right)
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 if there's land to the north
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 if there's land to the west
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+
+            perimeter += sum(edges)
 
     return perimeter
